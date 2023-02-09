04e53ab54989986b2ac1d069f117631778e9aabe
SarC-1 <dylanc.c.h@gmail.com>
2023-02-08 23:24:18 -0500

    Editado por Victor Dylan Contreras Castañeda

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-
+        //Editado por Victor Dylan Contreras Castañeda
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
         toyo.avanzar();
